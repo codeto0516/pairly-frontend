c0c3d527b1d14c39ad3cf0980b5b9b6da2cdee85
codeto0516 <codetaisei@gmail.com>
2023-09-06 20:57:14 +0900

    debug

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,9 @@ import { NextRequest, NextResponse } from "next/server";
 export default withAuth(
     (req: NextRequest) => {
         // console.log(req.url);
-
         // if (req.url === "http://localhost:3000/signin") {
         //     if (req.nextauth?.token) {
         //         console.log("ログイン済みです");
-
         //         // ログイン済みの場合
         //         return NextResponse.redirect("http://localhost:3000/"); // ホームにリダイレクト
         //     }
@@ -17,7 +15,9 @@ export default withAuth(
     {
         callbacks: {
             authorized: ({ token }) => {
-                return token ? true : false;
+                console.log(token);
+                // return token ? true : false;
+                return true;
             },
         },
         pages: {
