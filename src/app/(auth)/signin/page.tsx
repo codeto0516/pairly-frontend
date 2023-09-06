16f6a9d471c147b9843e0dfce97eefa9178c21f7
codeto0516 <codetaisei@gmail.com>
2023-09-06 20:38:49 +0900

    コンポーネントを分離

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,64 +1,7 @@
-"use client";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { Alert, LoadingButton } from "@mui/lab";
-import { useAuth } from "@/src/hooks/useAuth";
-import { Suspense, useEffect, useState } from "react";
-import { MailField, PasswordField } from "@/src/components/elements/form/TextField";
-import Loading from "../../loading";
+import { SignIn } from "./Signin";
 
-const page = () => {
+const Page = () => {
     return <SignIn />;
 };
 
-export default page;
-
-const SignIn = () => {
-    const router = useRouter();
-    const { signIn, isLoading, signInWithGoogle } = useAuth();
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setErrorMessage(null); // エラーメッセージをリセット
-        const data = new FormData(event.currentTarget);
-        const email = data.get("email")?.toString();
-        const password = data.get("password")?.toString();
-        if (email && password) {
-            try {
-                const res = await signIn(email, password);
-            } catch (error: any) {
-                console.log(error);
-                
-                setErrorMessage(error.message);
-            }
-        }
-    };
-
-    return (
-        <Suspense fallback={<Loading />}>
-            <div className="mt-2 flex flex-col items-center gap-4 w-3xl">
-                <h1 className="text-xl">ログイン</h1>
-                <button onClick={signInWithGoogle}>Googleでログイン</button>
-
-                <form onSubmit={handleSubmit} className="mt-3 flex flex-col gap-3 w-full  sm:w-[380px] ">
-                    <MailField />
-                    <PasswordField />
-                    <LoadingButton type="submit" fullWidth variant="outlined" className="h-14" loading={isLoading}>
-                        ログイン
-                    </LoadingButton>
-                    {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
-                </form>
-
-                <div className="w-full px-4">
-                    <Link href="/signup" className="text-sm text-blue-800 flex justify-end mt-4">
-                        パスワードを忘れた方はこちら
-                    </Link>
-                    <Link href="/signup" className="text-sm text-blue-800 flex justify-end mt-4">
-                        まだアカウントを登録していない方はこちら
-                    </Link>
-                </div>
-            </div>
-        </Suspense>
-    );
-};
+export default Page;
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,68 +1,7 @@
-"use client";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { MailField, ConfirmPasswordField, PasswordField } from "../../../components/elements/form/TextField";
-import { SubmitHandler, useForm } from "react-hook-form";
-import { Alert, LoadingButton } from "@mui/lab";
-import { useEffect, useState } from "react";
-import { useAuth } from "@/src/hooks/useAuth";
+import { SignUp } from "./Signup";
 
-const page = () => {
-    return <SignUp />;
+const Page = ({ searchParams }: { searchParams: { token: string } }) => {
+    return <SignUp  />;
 };
 
-export default page;
-
-interface SingUpInputs {
-    email: string;
-    password: string;
-    confirmPassword: string;
-}
-
-const SignUp = () => {
-    const { signInWithGoogle, signUp } = useAuth();
-
-    const [errorMessage, setErrorMessage] = useState<string | null>(null);
-
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setErrorMessage(null); // エラーメッセージをリセット
-        const data = new FormData(event.currentTarget);
-        const email = data.get("email")?.toString();
-        const password = data.get("password")?.toString();
-        const confirmPassword = data.get("password")?.toString();
-        if (password === confirmPassword && email && password) {
-            try {
-                await signUp(email, password);
-            } catch (error: any) {
-                console.log(error);
-
-                setErrorMessage(error.message); // エラーメッセージをセット
-            }
-        } else {
-            alert("パスワードが一致しません！");
-        }
-    };
-
-    return (
-        <div className="mt-2 flex flex-col items-center gap-4 w-full max-w-[360px]">
-            <button onClick={signInWithGoogle}>Googleで新規登録</button>
-            <h1 className="text-xl mb-8">新規登録</h1>
-            <form onSubmit={handleSubmit} className="mt-3 flex flex-col gap-3 w-full">
-                <MailField />
-                <PasswordField />
-                <ConfirmPasswordField />
-                <LoadingButton type="submit" fullWidth variant="outlined" className="h-14">
-                    登録
-                </LoadingButton>
-
-                {errorMessage && <Alert severity="error">{errorMessage}</Alert>}
-            </form>
-            <div className="w-full">
-                <Link href="/signin" className="text-sm text-blue-800 flex justify-end mt-4">
-                    既にアカウントを持っている方はこちら
-                </Link>
-            </div>
-        </div>
-    );
-};
+export default Page;
