d71d6085ff6782ae0adbfcecf493cdc2838607eb
codeto0516 <codetaisei@gmail.com>
2023-09-07 08:23:24 +0900

    型定義ファイルの拡張子を.d.tsに変更

--- src/hooks/api/v1/useTransaction.ts
+++ src/hooks/api/v1/useTransaction.ts
@@ -1,4 +1,4 @@
-import { TransactionType } from "@/src/types/transaction";
+import { TransactionType } from "@/src/types/transaction.d";
 import { useApi } from "./useApi";
 
 export const useTransaction = () => {
