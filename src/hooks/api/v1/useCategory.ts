932cfb14984bf08373f1447e18929d53c08ec683
codeto0516 <codetaisei@gmail.com>
2023-09-10 17:17:25 +0900

    不要なコメントアウトを削除

--- src/hooks/api/v1/useCategory.ts
+++ src/hooks/api/v1/useCategory.ts
@@ -7,16 +7,11 @@ export const useCategory = () => {
 
     const endPoint = urlJoin(process.env.NEXT_PUBLIC_API_BASE_URL, "categories");
 
-    // const endPoint = urlJoin("https://api.pairly.life/api/v1", "categories");
-    // const endPoint = urlJoin("http://localhost/api/v1", "categories");
-
     const getCategories = async (type: "spending" | "income"): Promise<any> => {
         const res: any = await api.get({
-            // url: `${endPoint}/${type}`,
             url: urlJoin(endPoint, type),
-            // cache: "force-cache",
+            cache: "force-cache",
         });
-        console.log(await res);
 
         return await res;
     };
