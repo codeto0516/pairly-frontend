0f79783c0ce9adc5b5f47f135f2b71adbbdb0628
codeto0516 <codetaisei@gmail.com>
2023-09-13 17:36:14 +0900

    User型を定義

--- src/types/user.d.ts
+++ src/types/user.d.ts
@@ -1,6 +1,17 @@
-export interface UserType {
-    id?: number;
-    userName?: string;
-    iconPath?: string;
-    partnerId?: number;
+
+interface Partner {
+    localId: string;
+    email: string;
+    displayName?: string | null;
+    photoUrl?: string | null;
 }
+
+export interface User {
+    localId: string;
+    email: string;
+    displayName?: string | null;
+    photoUrl?: string | null;
+    partner?: Partner | null | undefined;
+    token: string;
+}
+
