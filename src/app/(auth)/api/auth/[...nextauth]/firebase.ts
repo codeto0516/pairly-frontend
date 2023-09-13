7e65d84f65b9c6469540cca40276a5f8dd7eda4a
codeto0516 <codetaisei@gmail.com>
2023-09-13 17:22:15 +0900

    sessionでtokenを取得できるように変更

--- src/app/(auth)/api/auth/[...nextauth]/firebase.ts
+++ src/app/(auth)/api/auth/[...nextauth]/firebase.ts
@@ -1,19 +1,33 @@
-import type { NextAuthOptions } from "next-auth";
+import type { NextAuthOptions, Session, User } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import { JWT } from "next-auth/jwt";
 
-/////////////////////////////////////////////////////////////////
-// 設定
-/////////////////////////////////////////////////////////////////
+declare module "next-auth" {
+    interface Session {
+        token: {} | null;
+    }
+}
+
 export const authOptions: NextAuthOptions = {
     providers: [
         CredentialsProvider({
             credentials: {},
             authorize: async ({ idToken }: any, req): Promise<any | null> => {
-                return idToken ? idToken : null;
+                return { idToken };
             },
         }),
     ],
     session: {
         strategy: "jwt",
+    },
+    callbacks: {
+        jwt: async ({ token, user }: { token: JWT; user: User }) => {
+            return { ...token, ...user };
+        },
+        // sessionにJWTトークンからのユーザ情報を格納
+        session: async ({ session, token }: { session: Session; token: JWT }) => {
+            session.token = token.idToken ?? null;
+            return session;
+        },
     },
 };
