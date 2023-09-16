2caee1ed78030cb7c530bffefcfbad456fd35c0b
codeto0516 <codetaisei@gmail.com>
2023-09-16 14:05:41 +0900

    取引新規登録フォームの修正 Fixes #25

--- src/app/(private)/(home)/components/TransactionForm/Amount.tsx
+++ src/app/(private)/(home)/components/TransactionForm/Amount.tsx
@@ -1,6 +1,6 @@
 import TextField from "@mui/material/TextField";
 import { InputAdornment, Skeleton } from "@mui/material";
-import React, { ChangeEvent, useEffect, useState } from "react";
+import React, { ChangeEvent, useEffect, useRef, useState } from "react";
 import { Amount, Transaction } from "../../types/transaction";
 import { UserIcon } from "@/src/components/dataDisplay/UsersIcon";
 import { useTransactionContext } from "./main";
@@ -8,7 +8,7 @@ import { useUser } from "@/src/hooks/useUser";
 import { User } from "@/src/types/user";
 
 export const TransactionFormAmount = () => {
-    const { transaction, setTransaction } = useTransactionContext();
+    const { transaction } = useTransactionContext();
 
     return (
         <div className="flex flex-col gap-2">
@@ -25,9 +25,11 @@ export const TransactionFormAmount = () => {
 };
 
 const BaseAmountForm = (props: { item: any }) => {
-    const { _, setTransaction } = useTransactionContext();
+    const { setTransaction } = useTransactionContext();
     const [user, setUser] = useState<User | null>(null);
     const { getUser } = useUser();
+    const [value, setValue] = useState(props.item.amount);
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const changeAmount = (e: ChangeEvent<HTMLInputElement>) => {
         const userId = props.item.userId;
@@ -46,9 +48,18 @@ const BaseAmountForm = (props: { item: any }) => {
             const user: any = await getUser(props.item.userId);
             setUser(() => user);
         })();
-
-        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
+
+    useEffect(() => {
+        if (props.item.amount === 0) {
+            setValue(() => null);
+            if (inputRef.current) {
+                inputRef.current.value = "";
+            }
+        } else {
+            setValue(props.item.amount);
+        }
+    }, [props.item.amount]);
 
     if (user == null) {
         return (
@@ -65,17 +76,14 @@ const BaseAmountForm = (props: { item: any }) => {
             <UserIcon label={user.displayName ?? user.email} image={user.photoUrl} />
             {/* 入力フォーム */}
             <TextField
+                inputRef={inputRef}
                 label={`${user.displayName ?? user.email} の支払い額を入力`}
                 variant="standard"
                 className="w-full"
                 type="number"
-                value={props.item.amount <= 0 ? null : props.item.amount}
+                value={value}
                 onChange={changeAmount}
                 InputProps={{
-                    inputProps: {
-                        inputMode: "numeric",
-                        pattern: "[0-9]*", // 数値以外の文字を削除
-                    },
                     startAdornment: <InputAdornment position="start">¥</InputAdornment>,
                 }}
             />
--- src/app/(private)/(home)/components/TransactionForm/main.tsx
+++ src/app/(private)/(home)/components/TransactionForm/main.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useContext, useState, createContext, useEffect, useCallback, memo } from "react";
+import { useContext, useState, createContext, useEffect, useCallback, memo, SetStateAction } from "react";
 
 // Material UI
 import { Button } from "@mui/material";
@@ -11,7 +11,7 @@ import { Transaction } from "../../types/transaction";
 import { useToggle } from "@/src/hooks/useToggle";
 import { LoadingButton } from "@mui/lab";
 import { useTransaction } from "@/src/app/(private)/(home)/api/useTransaction";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+import { useRecoilState } from "recoil";
 import { TransactionFormDate } from "./Date";
 import { TransactionFormCategory } from "./Category";
 import { TransactionFormContent } from "./Content";
@@ -19,7 +19,6 @@ import { TransactionFormAmount } from "./Amount";
 import { DeleteDialog } from "@/src/components/utils/Dialog";
 import {
     transactionListState,
-    transactionListSelector,
     addTransaction as addTransactionAction,
     updateTransaction as updateTransactionAction,
     deleteTransaction as deleteTransactionAction,
@@ -32,95 +31,36 @@ export const useTransactionContext = () => useContext(TransactionContext);
 // 本体
 //////////////////////////////////////////////////////////////////////////////////
 
-export const TransactionForm = (props: { transaction: Transaction }) => {
-    const [isDialog, toggleDialog] = useToggle(false);
-    const [isLoading, toggleLoading] = useToggle(false);
-    const [isButtonDisable, toggleButtonDisable] = useToggle(true);
+export const TransactionForm = (props: { transaction: Transaction; toggleModal?: (value: boolean) => void }) => {
+    // 新規取引かどうか
+    const isNewTransaction = props.transaction.id ? false : true;
 
-    const { createTransaction, updateTransaction, delteTransaction } = useTransaction();
-
-    const [isNewTransaction] = useState(props.transaction.id ? false : true);
-
+    // 取引の状態
     const [transaction, setTransaction] = useState(props.transaction);
     const changeTransaction = useCallback((field: string, value: string | number) => {
         setTransaction((prevTransaction) => ({ ...prevTransaction, [field]: value }));
     }, []);
 
-    const [transactionList, setTransactionList] = useRecoilState<any>(transactionListState);
-
-    // 保存ボタンを押したらサーバーに送信
-    const handleSave = async () => {
-        toggleLoading(true);
-        const res = await createTransaction(transaction);
-        if (res.ok) {
-            const newTransactionList = addTransactionAction(res.data.transaction)(transactionList);
-            setTransactionList(newTransactionList);
-            toggleButtonDisable(true);
-        }
-        toggleLoading(false);
-    };
-
-    const handleUpdate = async () => {
-        toggleLoading(true);
-        const res: any = await updateTransaction(transaction);
-        if (res.ok) {
-            const updatedTransactionList = updateTransactionAction(res.data.transaction)(transactionList);
-            setTransactionList(updatedTransactionList);
-            toggleButtonDisable(true);
-        }
-        toggleLoading(false);
-    };
-
-    const handleDelete = async () => {
-        if (transaction.id) {
-            const res = await delteTransaction(transaction.id);
-            if (res.ok) {
-                const updatedTransactionList = deleteTransactionAction(transaction.id)(transactionList);
-                setTransactionList(updatedTransactionList);
-            }
-        }
-    };
-
-    useEffect(() => {
-        const totalAmout = transaction.amounts.reduce((sum: any, entry: any) => sum + entry.amount, 0);
-        // フォームが編集されたら検知して保存ボタンのDisabledを解除
-        totalAmout === 0 ? toggleButtonDisable(true) : toggleButtonDisable(false);
-    }, [transaction]);
-
     //////////////////////////////////////////////////////////////////////////////////
     return (
         <TransactionContext.Provider value={{ transaction, changeTransaction, setTransaction }}>
             <div className="flex flex-col gap-2 text-sx bg-white w-full px-6 py-8">
-                {/* 削除ボタンと日付を水平に配置する */}
-                <div className="flex justify-between items-center">
-                    {/* 削除ボタン */}
-                    {isNewTransaction ? (
-                        <>
-                            <Button className="text-sm text-blue-500" onClick={() => toggleDialog(true)}>
-                                清算する
-                            </Button>
-                            {/* <DeleteDialog
-                                open={isDialog}
-                                handleExec={handleDelete}
-                                handleClose={() => toggleDialog(false)}
-                            /> */}
-                        </>
-                    ) : (
-                        <>
-                            <Button className="text-sm text-red-500" onClick={() => toggleDialog(true)}>
-                                削除する
-                            </Button>
-                            <DeleteDialog
-                                open={isDialog}
-                                handleExec={handleDelete}
-                                handleClose={() => toggleDialog(false)}
-                            />
-                        </>
-                    )}
+             
 
-                    {/* 日付 */}
-                    <TransactionFormDate />
-                </div>
+                {isNewTransaction ? (
+                    <div className="flex justify-end items-center">
+                        {/* 日付 */}
+                        <TransactionFormDate />
+                    </div>
+                ) : (
+                    <div className="flex justify-between items-center">
+                        {/* 削除ボタン */}
+                        <DeleteButton transaction={transaction} />
+
+                        {/* 日付 */}
+                        <TransactionFormDate />
+                    </div>
+                )}
 
                 {/* カテゴリー選択ボタン */}
                 <TransactionFormCategory />
@@ -131,26 +71,140 @@ export const TransactionForm = (props: { transaction: Transaction }) => {
                 {/* 購入内容等の入力フォーム */}
                 <TransactionFormContent />
 
+                {/* ボタン */}
                 {isNewTransaction ? (
-                    <LoadingButton
-                        variant="outlined"
-                        onClick={handleSave}
-                        loading={isLoading}
-                        disabled={isButtonDisable}
-                    >
-                        保存
-                    </LoadingButton>
+                    <SaveButton transaction={transaction} setTransaction={setTransaction} />
                 ) : (
-                    <LoadingButton
-                        variant="outlined"
-                        onClick={handleUpdate}
-                        loading={isLoading}
-                        disabled={isButtonDisable}
-                    >
-                        変更
-                    </LoadingButton>
+                    <UpdateButton transaction={transaction} />
                 )}
             </div>
         </TransactionContext.Provider>
+    );
+};
+
+///////////////////////////////////////////////////////////////////////////////////////
+// 保存ボタン
+///////////////////////////////////////////////////////////////////////////////////////
+const SaveButton = (props: {
+    toggleModal?: (value: boolean) => void;
+    transaction: Transaction;
+    setTransaction: (value: React.SetStateAction<Transaction>) => void;
+}) => {
+    // 取引の初期値を保持（保存後にフォームを初期化するため）
+    const initialTransaction = props.transaction;
+
+    // ローディング、ボタンの状態
+    const [isLoading, toggleLoading] = useToggle(false);
+    const [isButtonDisable, toggleButtonDisable] = useToggle(true);
+
+    // API
+    const { createTransaction } = useTransaction();
+
+    // 取引リストの状態
+    const [transactionList, setTransactionList] = useRecoilState<any>(transactionListState);
+
+    useEffect(() => {
+        const totalAmout = props.transaction.amounts.reduce((sum: any, entry: any) => sum + entry.amount, 0);
+        // フォームが編集されたら検知して保存ボタンのDisabledを解除
+        totalAmout === 0 ? toggleButtonDisable(true) : toggleButtonDisable(false);
+    }, [props.transaction.amounts]);
+
+    // 保存
+    const handleSave = async () => {
+        // ローディングを表示
+        toggleLoading(true);
+        const res = await createTransaction(props.transaction);
+        if (res.ok) {
+            // 保存ボタンを無効化
+            toggleButtonDisable(true);
+
+            // レスポンスされた取引を取引リストに追加
+            const newTransactionList = addTransactionAction(res.data.transaction)(transactionList);
+            setTransactionList(newTransactionList);
+
+            // フォームを初期化
+            props.setTransaction(initialTransaction);
+
+            // モーダルを閉じる
+            props.toggleModal?.(false);
+        }
+        // ローディングを非表示
+        toggleLoading(false);
+    };
+
+    return (
+        <LoadingButton variant="outlined" onClick={handleSave} loading={isLoading} disabled={isButtonDisable}>
+            保存
+        </LoadingButton>
+    );
+};
+
+///////////////////////////////////////////////////////////////////////////////////////
+// 更新ボタン
+///////////////////////////////////////////////////////////////////////////////////////
+const UpdateButton = (props: { transaction: Transaction }) => {
+    const { updateTransaction } = useTransaction();
+
+    const [isLoading, toggleLoading] = useToggle(false);
+    const [isButtonDisable, toggleButtonDisable] = useToggle(true);
+
+    // 取引リストの状態
+    const [transactionList, setTransactionList] = useRecoilState<any>(transactionListState);
+
+    // 更新
+    const handleUpdate = async () => {
+        // ローディングを表示
+        toggleLoading(true);
+        const res: any = await updateTransaction(props.transaction);
+        if (res.ok) {
+            const updatedTransactionList = updateTransactionAction(res.data.transaction)(transactionList);
+            setTransactionList(updatedTransactionList);
+            toggleButtonDisable(true);
+        }
+        toggleLoading(false);
+    };
+
+    useEffect(() => {
+        const totalAmout = props.transaction.amounts.reduce((sum: any, entry: any) => sum + entry.amount, 0);
+        // フォームが編集されたら検知して保存ボタンのDisabledを解除
+        totalAmout === 0 ? toggleButtonDisable(true) : toggleButtonDisable(false);
+    }, [props.transaction.amounts]);
+
+    return (
+        <LoadingButton variant="outlined" onClick={handleUpdate} loading={isLoading} disabled={isButtonDisable}>
+            変更
+        </LoadingButton>
+    );
+};
+
+///////////////////////////////////////////////////////////////////////////////////////
+// 削除ボタン
+///////////////////////////////////////////////////////////////////////////////////////
+const DeleteButton = (props: { transaction: Transaction }) => {
+    const { delteTransaction } = useTransaction();
+
+    // ダイアログ、ローディング、ボタンの状態
+    const [isDialog, toggleDialog] = useToggle(false);
+
+    // 取引リストの状態
+    const [transactionList, setTransactionList] = useRecoilState<any>(transactionListState);
+
+    // 削除
+    const handleDelete = async () => {
+        if (props.transaction.id) {
+            const res = await delteTransaction(props.transaction.id);
+            if (res.ok) {
+                const updatedTransactionList = deleteTransactionAction(props.transaction.id)(transactionList);
+                setTransactionList(updatedTransactionList);
+            }
+        }
+    };
+    return (
+        <div>
+            <Button className="text-sm text-red-500" onClick={() => toggleDialog(true)}>
+                削除する
+            </Button>
+            <DeleteDialog open={isDialog} handleExec={handleDelete} handleClose={() => toggleDialog(false)} />
+        </div>
     );
 };
